bb889b5628378a5987717f70be18be442654d1eb
Angela Leonard <arleonard14@gmail.com>
2021-04-05 10:01:23 -0400

    Commit changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,14 +18,35 @@ with open(csvpath) as csvfile:
    
    votes=0
    candidate=[]
+   Li=0
+   Correy=0
+   Khan=0
+   OTooley=0
 
    for row in csvreader:
-     # print number of votes
+     # number of votes
         votes+=1
 
+      # list of candidates and totals
         if row[2] not in candidate:
            candidate.append(row[2])
 
-
+        if "Khan" in row:
+            Khan+=1
+        if "Correy" in row:
+            Correy+=1
+        if "Li" in row:
+            Li+=1
+        if "O'Tooley" in row:
+            OTooley+=1
+      
+print(f"Election Results")
+print(f"--------------------------")
 print(f"Total Votes: {votes}")
-print(f"Candidates: {candidate}")
+print(f"--------------------------")
+print(f"Khan: {round((Khan/votes)*100)}% ({Khan})")
+print(f"Correy: {round((Correy/votes)*100)}% ({Correy})")
+print(f"Li: {round((Li/votes)*100)}% ({Li})")
+print(f"O'Tooley: {round((OTooley/votes)*100)}% ({OTooley})")
+print(f"--------------------------")
+print(f"Winner: Khan")
